a31a7efae1d30d22cdf7f4a20d4485643f20c914
Shubham <vijit101@gmail.com>
2020-04-29 19:07:40 +0530

    - Day 1 - Random Food throw

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public float spawnRate = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTargetCor());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator SpawnTargetCor()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+        
+    }
+}
